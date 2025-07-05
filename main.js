a4d547c61a76e08cf64b74a076cd65c16cbc32fc
Hamid00777845 <salomatlik87@gmail.com>
2025-07-05 16:24:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const kunlar =[12, 13, 101, 105, 40, 24, 52, 70, 80, 75, 48, 107,]
+
+const juft =[]
+
+const toq = []
+
+for (let i = 0; i < kunlar.length; i++) {
+    if (kunlar [i] % 2 === 0) {
+    juft.push (kunlar[i]);
+ }
+
+ else {
+    toq.push (kunlar[i]);
+ }
+}
+console.log(juft);
+console.log(toq);
+
+
+ 
